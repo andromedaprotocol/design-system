d74a225ee1e3d364a319346ae329851ec2cc38ad
jmsaulnier <jmsaulnier.dev@gmail.com>
2022-03-15 05:54:13 +0000

    Deploying to gh-pages from @ andromedaprotocol/design-system@f3a1c0e5b7ab262e79fdccfda5e38f04f7af3941 🚀

--- libs/react-feather/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Github } from "./github";
-export { default as Twitter } from "./twitter";
--- next-sitemap.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  siteUrl: "https://andromedaprotocol.github.io/design-system",
-};
