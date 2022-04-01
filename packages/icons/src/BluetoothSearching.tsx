4492a8237a2f44daf8eff5ba08c12791a1f6319c
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2022-03-31 22:49:17 -0700

    update icons template

--- packages/icons/src/BluetoothSearching.tsx
+++ packages/icons/src/BluetoothSearching.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 
-export const BluetoothSearching = React.forwardRef<SVGSVGElement, React.ComponentPropsWithRef<"svg">>(
+import { DefaultProps } from "@andromedaprotocol/theme";
+
+export interface IconProps extends DefaultProps {
+  as?: React.ElementType;
+  inline?: boolean;
+  role?: string;
+  color?: string;
+  /**
+   * The accessible label for the icon. This label will be visually hidden but announced to screen
+   * reader users, similar to `alt` text for `img` tags.
+   */
+  label: string;
+  name: string;
+}
+
+export const BluetoothSearching = React.forwardRef<any, IconProps>(
   ({ color = "currentColor", ...props }, ref) => {
     return (
         <svg fill="currentcolor" stroke={color} {...props} ref={ref}>
