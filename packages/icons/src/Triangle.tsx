f27eb9831882df8e37e911422c6ef562885f78b5
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2022-03-31 22:52:26 -0700

    update templace icon

--- packages/icons/src/Triangle.tsx
+++ packages/icons/src/Triangle.tsx
@@ -1,21 +1,8 @@
 import React from "react";
+import { SVGProps } from "react";
+import { IconProps } from "@andromedaprotocol/icon";
 
-import { DefaultProps } from "@andromedaprotocol/theme";
-
-export interface IconProps extends DefaultProps {
-  as?: React.ElementType;
-  inline?: boolean;
-  role?: string;
-  color?: string;
-  /**
-   * The accessible label for the icon. This label will be visually hidden but announced to screen
-   * reader users, similar to `alt` text for `img` tags.
-   */
-  label: string;
-  name: string;
-}
-
-export const Triangle = React.forwardRef<any, IconProps>(
+export const Triangle = React.forwardRef<SVGSVGElement, IconProps>(
   ({ color = "currentColor", ...props }, ref) => {
     return (
         <svg fill="currentcolor" stroke={color} {...props} ref={ref}>
